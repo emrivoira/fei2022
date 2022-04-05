8db2fb9d649a7bdd23f22b911c9c5e63a5d3e0ee
emrivoira <emrivoira@gmail.com>
2022-04-05 02:29:58 -0300

    Tutorial 3-Primer Migracion

---
+++ src/basic/migrations/m220405_051656_crear_tabla_usuarios.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220405_051656_crear_tabla_usuarios
+ */
+class m220405_051656_crear_tabla_usuarios extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('usuarios', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull(),
+            'nombre' => $this->text(),
+            'apellido' => $this->text(),
+            'edad' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('usuarios');
+
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220405_051656_crear_tabla_usuarios cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
